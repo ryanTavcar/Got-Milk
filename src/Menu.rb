a0c2b12a57a045687b875865036424c41cc45cfa
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2020-12-15 16:42:10 +1100

    added another method for navigating to a menu.

--- src/Menu.rb
+++ src/Menu.rb
@@ -3,7 +3,7 @@ require_relative './Convert.rb'
 require_relative './Help.rb'
 
 
-
+# Menu class that will provide all menu-ing options throughout the application
 class Menu
     def initialize
         @answers = {
@@ -11,6 +11,9 @@ class Menu
             2 => ["2", "convert"],
             3 => ["3", "help"]
         }
+        @recipe = Recipe.new
+        @convert = Convert.new
+        @help = Help.new
     end
 
     def start() # Start Menu class / main code block
@@ -19,16 +22,29 @@ class Menu
         user_input = gets.chomp.downcase
         
         if @answers[1].include?(user_input)
-            recipe = Recipe.new
-            recipe.start
+            @recipe.start
         elsif @answers[2].include?(user_input)
-            convert = Convert.new.start
+            @convert.start
         elsif @answers[3].include?(user_input)
-            help = Help.new.start
+            @help.start
         elsif user_input == "end"
             exit 
         else
             puts "Thats wasn't a valid input, type 1 to start program, type 2 to navigate to the conversion feature and type 3 to see help."
+        end
+    end
+
+    def ask_for_conversion
+        puts ""
+        puts "What would you like to do?"
+        puts "- [1] Download recipe\n- [2] See Conversion tool\n- [3] Back to main menu "
+        user_convert_input = gets.chomp
+        if user_convert_input == "1"
+            @recipe.download_recipe
+        elsif user_convert_input == "2"
+            @convert.start
+        elsif user_convert_input == "3"
+            self.start
         end
     end
 end
