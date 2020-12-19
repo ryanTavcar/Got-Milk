67716f4c857de3b0e7818d10464670c28ac625e9
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2020-12-19 19:19:28 +1100

    added shell scripts.

--- src/index.rb
+++ src/index.rb
@@ -1,10 +1,36 @@
 require_relative './Menu.rb'
 require_relative './Recipe.rb'
-#require_relative './Convert.rb'
+require_relative './Convert.rb'
 
+convert = Convert.new
 menu = Menu.new
-menu.start_main_menu
-#recipe = Recipe.new
-#recipe.get_json
+
+if ARGV.empty? == true
+    menu.start_main_menu
+end
+
+case ARGV.empty? == false
+
+when ARGV[0] == "lbs to kg"
+    puts "#{convert.lbs_to_kg(ARGV[1])} kg"
+when ARGV[0] == "kg to lbs"
+    puts "#{convert.kg_to_lbs(ARGV[1])} lbs"
+when ARGV[0] == "ounce to grams"
+    puts "#{convert.oz_to_g(ARGV[1])} g"
+when ARGV[0] == "grams to ounce"
+    puts "#{convert.g_to_oz(ARGV[1])} oz"
+when ARGV[0] == "gallon to litre"
+    puts "#{convert.gallon_to_litre(ARGV[1])} litre"
+when ARGV[0] == "litre to gallon"
+    puts "#{convert.litre_to_gallon(ARGV[1])} gallon"
+when ARGV[0] == "quartz to ml"
+    puts "#{convert.quart_to_ml(ARGV[1])} ml"
+when ARGV[0] == "ml to quartz"
+    puts "#{convert.ml_to_quartz(ARGV[1])} quartz"
+else
+    menu.start_main_menu
+end
+
+
 
 
